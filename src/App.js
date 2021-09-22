b60c0abc21c920d54f7aa1ae8b000f82aaab7d5d
dwainejade <dwainejade@gmail.com>
2021-09-22 15:02:43 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <input type="text" />
+      <button>Add Todo</button>
+    </div>
+  );
+}
+
+export default App;
