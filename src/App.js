2211b21f93318908964e44d1280657209fc4f08c
Dwaine Matthew <46469243+dwainejade@users.noreply.github.com>
2021-09-22 16:41:03 -0400

    todos created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Todo from "./Todo";
+import db from "./firebase";
+
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([input, ...todos]);
+    setInput("");
+    console.log(input);
+  };
+
+  useEffect(() => {
+    db.collection("todos").onSnapshot((snapshot) => {
+      setTodos(snapshot.docs.map((doc) => doc.data().todo));
+    });
+  }, []);
+
   return (
     <div className="App">
-      <h1>Todo App</h1>
-      <input type="text" />
-      <button>Add Todo</button>
+      <h1>Todo List</h1>
+      <form>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={addTodo} disabled={!input}>
+          Add Todo
+        </button>
+        <ul>
+          {todos.map((todo) => (
+            <Todo text={todo} />
+          ))}
+        </ul>
+      </form>
     </div>
   );
 }
